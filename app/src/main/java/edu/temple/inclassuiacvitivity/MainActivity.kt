ba197a9e98a6d76c43a37555175085bf23eff55c
Erin Santosa <tun35949@temple.edu>
2023-09-19 11:42:46 -0400

    initialize & populate number array with 100 numbers

--- app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
@@ -14,10 +14,11 @@ class MainActivity : AppCompatActivity() {
         val displayTextView = findViewById<TextView>(R.id.textDisplay)
 
         /* Step 1: Populate this array */
-        //val numberArray = Array Of Numbers
+        val numberArray = IntArray(100) {it * 1}
+
 
         /* Step 2: Create adapter to display items from array in Spinner */
-        //spinner.adapter = ArrayAdapter...
+
 
 
         // Step 3: Change TextView's text size to the number selected in the Spinner */
