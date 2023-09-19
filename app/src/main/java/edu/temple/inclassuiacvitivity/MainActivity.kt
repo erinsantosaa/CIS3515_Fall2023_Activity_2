069f9603829cef3e1ef2264d574201ad9cbf8408
Erin Santosa <tun35949@temple.edu>
2023-09-19 11:53:53 -0400

    fixed numberArray to use Array instead of intArray & created adapter to display array items in spinner

--- app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
@@ -14,15 +14,15 @@ class MainActivity : AppCompatActivity() {
         val displayTextView = findViewById<TextView>(R.id.textDisplay)
 
         /* Step 1: Populate this array */
-        val numberArray = IntArray(100) {it * 1}
-
+        val numberArray = Array(100) {it * 1}
 
         /* Step 2: Create adapter to display items from array in Spinner */
-
-
+        val adapter = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, numberArray)
+        spinner.adapter = adapter
 
         // Step 3: Change TextView's text size to the number selected in the Spinner */
-        //spinner.onItemSelectedListener = object: ...
+
 
     }
+
 }
